73f9f64549aa8165f9f8c75c0bd099eacf8f8be5
Taylor Treece <TTreece@wmcarey.edu>
2025-10-11 20:52:01 -0500

    added playing status

--- public/js/current-song.js
+++ public/js/current-song.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', async () => {
   try {
     const r = await fetch('https://fantastic-manatee-a1d0c8.netlify.app/.netlify/functions/api/getCurrentlyListening', { headers:{Accept:'application/json'} });
     if (!r.ok) throw new Error(r.status);
-    const { title = '', artist = '', imageUrl = '' } = await r.json();
+    const { title = '', artist = '', imageUrl = '',  isCurrentlyListening} = await r.json();
+    statusText = 'Not Currently<br />Playing 🔴';
+    if (!!isCurrentlyListening) {
+      statusText = 'Listening Now 🟢';
+    }
 
     document.getElementById('banner').innerHTML = `
       <div class="now-playing">
@@ -10,6 +14,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         <div class="now-playing-text">
           <p class="title">${title}</p>
           <p class="artist">${artist}</p>
+        </div>
+        <div class="now-playing-status">
+          <p class="status-text">${statusText}</p>
         </div>
       </div>`;
   } catch (e) {
